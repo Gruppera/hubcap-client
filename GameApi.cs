49355d7b0718a45a964e5e7dce4cb2326c1184b9
Daniel Bohlin <daniel.bohlin@gruppera.se>
2018-11-30 10:57:55 +0100

    initial commit

---
+++ GameApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Hubcap.TestClient
+{
+    public static class GameApi
+    {
+        public static HttpClient Client { get; } = new HttpClient(){Timeout = new TimeSpan(0,2,0,0)};
+
+        static GameApi()
+        {
+            //Client.BaseAddress = new Uri("https://localhost:44345/");
+            Client.BaseAddress = new Uri("http://hubcap.gruppera.net/");
+        }
+    }
+}
